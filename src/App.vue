78df051b9ada80074ab645fdd6b8b729f2632f8f
Tutku Uçan <t.ucan@equota.de>
2023-02-13 20:15:47 +0300

    Update example page

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,17 @@
       <div class="content">
         <h1>vue-color-mode</h1>
         <p>Simple and easy to integration theme mode plugin for Vue</p>
-        <a href="https://github.com/tutkuofnight/vue-color-mode#vue-color-mode" target="_blank">Read docs</a>
+        <a href="https://github.com/tutkuofnight/vue-color-mode#vue-color-mode" target="_blank">Documentation</a>
+        <a href="https://github.com/tutkuofnight/vue-color-mode/blob/main/src/App.vue" target="_blank">Example</a>
         <div>
           <button @click="$setColorMode('dark')">Dark Mode</button>
           <button @click="$setColorMode('sepia')">Sepia Mode</button>
           <button @click="$setColorMode()">Default</button>
         </div>
       </div>
+      <footer>
+        <p>Developed by <a href="https://github.com/tutkuofnight">@tutkuofnight</a></p>
+      </footer>
     </main>
   </div>
 </template>
@@ -39,6 +43,7 @@ export default {
 :root {
   --bgColor: white;
   --color: black;
+  --linkColor: blue;
 }
 body {
   background: var(--bgColor);
@@ -49,16 +54,19 @@ body {
 .dark-mode {
   --bgColor: black;
   --color: whitesmoke;
+  --linkColor: aqua;
 }
 .sepia-mode {
   --bgColor: #f1e7d0;
   --color: #433422;
+  --linkColor: crimson;
 }
 
 main {
   width: 100%;
   height: 100vh;
   display: grid;
+  grid-template-rows: 1fr;
   place-items: center;
 }
 main .content {
@@ -67,8 +75,15 @@ main .content {
   border-radius: 4px;
   padding: 30px 60px;
 }
+main footer {
+  margin: 20px 0px;
+}
 main .content  h1 , p {
   margin: 10px 0px;
+}
+a {
+  color: var(--linkColor);
+  margin: 10px 5px;
 }
 button {
   border: 2px solid var(--color);
